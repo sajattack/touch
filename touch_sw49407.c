4cf8eed36d9604022de8238d6596886d79d831be
Harrison Kim <kimhh@siliconworks.co.kr>
2017-04-12 14:43:39 +0900

    input: touchscreen: siw: SW49407 entry - remove tci_reset_area element
    Actually not used

--- touch_sw49407.c
+++ touch_sw49407.c
@@ -422,7 +422,6 @@ static const struct siw_touch_pdata chip_pdata = {
 	/* */
 	//See 'siw_hal_get_tci_info' [siw_touch_hal.c]
 	.tci_info			= __CHIP_TCI_INFO,
-	.tci_reset_area		= NULL,
 	.tci_qcover_open	= NULL,
 	.tci_qcover_close	= NULL,
 	//See 'siw_hal_get_swipe_info' [siw_touch_hal.c]
